546e64195c7d5a6393c0f6a509567de84b9af546
Phyungjin <null>
2019-01-26 09:19:18 +0000

    Add: Programmers.BigNumber

--- src/project/MyAlgorithm.java
+++ src/project/MyAlgorithm.java
@@ -1,9 +1,10 @@
 package project;
-import algorithm.my.sort.MyPermutation;
-
+// import algorithm.my.sort.MyPermutation;
+import algorithm.my.programmers.BigNumber;
 public class MyAlgorithm {
     public static void main(String[] args) {
-        MyPermutation perm = new MyPermutation();
-        perm.permutation();
+        // MyPermutation perm = new MyPermutation();
+        // perm.permutation();
+        new BigNumber();
     }
 }
