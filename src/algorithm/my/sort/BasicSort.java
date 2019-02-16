0f3f7439c7fa325559f34d8327819db7cf735e76
Phyungjin <null>
2019-02-16 12:29:04 +0000

    Add: bubble & selection Sort

---
+++ src/algorithm/my/sort/BasicSort.java
@@ -0,0 +1,38 @@
+package algorithm.my.sort;
+import java.util.Arrays;
+
+public class BasicSort {
+    public void bubbleSort(){
+        int[] arr = {9,5,1,6,2};
+        System.out.println("before "+Arrays.toString(arr));
+        for(int i=0 ; i<arr.length-1 ; i++) { // rotation num
+            for(int j=0 ; j<arr.length-i-1 ; j++) {
+                if(arr[j]>arr[j+1]) { //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }                    
+            }
+        }
+        System.out.println("after bubbleSort " + Arrays.toString(arr));
+    }
+    
+    public void selectionSort() {
+        int[] arr = {9,5,1,6,2};
+        System.out.println("before "+Arrays.toString(arr));
+        
+        for(int i=0; i<arr.length-1 ; i++) {
+            int pointer = i;
+            for(int j=i+1 ; j<arr.length ; j++) {
+                if(arr[pointer] > arr[j]){ //swap
+                    pointer = j;
+                }
+            }
+            int temp = arr[pointer];
+            arr[pointer] = arr[i];
+            arr[i] = temp;
+        }
+        
+        System.out.println("after selectionSort " + Arrays.toString(arr));
+    }
+}
--- src/project/MyAlgorithm.java
+++ src/project/MyAlgorithm.java
@@ -2,12 +2,16 @@ package project;
 // import algorithm.my.sort.MyPermutation;
 import algorithm.my.programmers.BigNumber;
 import algorithm.my.backjun.PrintStar;
+import algorithm.my.sort.BasicSort;
+
 public class MyAlgorithm {
     public static void main(String[] args) {
         // MyPermutation perm = new MyPermutation();
         // perm.permutation();
         // new BigNumber();
         // new PrintStar().printStar();
-        new PrintStar().printStar2();
+        // new PrintStar().printStar2();
+        // new BasicSort().bubbleSort();
+        new BasicSort().selectionSort();
     }
 }
