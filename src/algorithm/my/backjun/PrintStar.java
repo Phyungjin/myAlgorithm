06018c6cde14a1e95a7d87892f36a5d557153cf0
Phyungjin <null>
2019-02-16 11:31:34 +0000

    Add: Backjun 10991 & 10992

---
+++ src/algorithm/my/backjun/PrintStar.java
@@ -0,0 +1,60 @@
+package algorithm.my.backjun;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class PrintStar {
+    public void printStar() {//10991
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0; j<i+n ; j++) {
+               if(j>=n-i-1) {
+                   if((j-n+i+1) % 2 == 0)
+                       System.out.print("*");
+                   else
+                       System.out.print(" ");
+               }else {
+                   System.out.print(" ");
+               }
+            }
+            System.out.println();
+        } 
+    }
+    
+    public void printStar2() {//10992
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        int num1 = n-1;
+        int num2 = n-1;
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<n*2-1 ; j++) {
+                if(i < n-1) {
+                    if(j == num1 || j == num2) {
+                        System.out.print("*");
+                        if(j == num2){
+                            System.out.print(" ");
+                            break;
+                        }
+                    }else {
+                        System.out.print(" ");
+                    }
+                }else {
+                    System.out.print("*");
+                }
+            }
+
+            if(i < n-1){
+                System.out.println();
+                num1--;
+                num2++;
+                for(int j=0 ; j<=num2+1 ; j++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+
+            }
+        }
+    }
+}
--- src/project/MyAlgorithm.java
+++ src/project/MyAlgorithm.java
@@ -1,10 +1,13 @@
 package project;
 // import algorithm.my.sort.MyPermutation;
 import algorithm.my.programmers.BigNumber;
+import algorithm.my.backjun.PrintStar;
 public class MyAlgorithm {
     public static void main(String[] args) {
         // MyPermutation perm = new MyPermutation();
         // perm.permutation();
-        new BigNumber();
+        // new BigNumber();
+        // new PrintStar().printStar();
+        new PrintStar().printStar2();
     }
 }
